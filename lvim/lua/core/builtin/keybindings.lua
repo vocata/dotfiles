eb05be4f15da473b56cca6e338ca7f52fb94e60e
vocata <leixueyue@gmail.com>
2023-08-06 01:56:42 +0800

    lvim: remove old key binding of packer

--- lvim/lua/core/builtin/keybindings.lua
+++ lvim/lua/core/builtin/keybindings.lua
@@ -1,10 +1,3 @@
 -- Builtin key binding
 local builtin_mappings = lvim.builtin.which_key.mappings
 builtin_mappings["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment Toggle" }
-
--- Packer key binding
-table.insert(lvim.builtin.which_key.mappings["p"],
-  {
-    C = { "<cmd>PackerClean<cr>", "Clean" },
-  }
-)
