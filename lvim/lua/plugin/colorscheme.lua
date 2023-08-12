bd8ddd56a8262724fd86fedd534404af209cd421
leixueyue <leixueyue@bytedance.com>
2023-08-12 15:06:53 +0800

    lvim: load custom colorscheme at startup

--- lvim/lua/plugin/colorscheme.lua
+++ lvim/lua/plugin/colorscheme.lua
@@ -1,7 +1,6 @@
 table.insert(lvim.plugins,
   {
     "sainnhe/sonokai",
-    event = "VeryLazy",
     config = function()
       vim.g.sonokai_style = "atlantis"
       vim.g.sonokai_better_performance = 1
