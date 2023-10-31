c81dc98702a78f9dd50f6a532643bd3f874e090d
leixueyue <leixueyue@bytedance.com>
2023-11-01 01:23:50 +0800

    lvim: add lsp restart key binding

--- lvim/lua/core/builtin/keybindings.lua
+++ lvim/lua/core/builtin/keybindings.lua
@@ -1,3 +1,6 @@
 -- Builtin key binding
 local builtin_mappings = lvim.builtin.which_key.mappings
+
 builtin_mappings["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment Toggle" }
+
+builtin_mappings["l"].u = { "<cmd>LspRestart<cr>", "Restart" }
