0321f919b2b2f71c6369a5465dfd621490655278
leixueyue <leixueyue@bytedance.com>
2023-12-09 00:05:00 +0800

    lvim: add python formatter 'black' and 'isort'

--- lvim/config.lua
+++ lvim/config.lua
@@ -73,20 +73,28 @@ lvim.builtin.treesitter.auto_install = true
 -- end
 
 -- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-width", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  --   { name = "stylua" },
+  --   {
+  --     name = "prettier",
+  --     args = { "--print-width", "100" },
+  --     filetypes = { "typescript", "typescriptreact" },
+  --   },
+  {
+    name = "black",
+    filetypes = { "python" },
+  },
+  {
+    name = "isort",
+    filetypes = { "python" },
+  }
+}
 -- local linters = require "lvim.lsp.null-ls.linters"
 -- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
+--   { name = "flake8", filetypes = { "python" } },
 --   {
---     command = "shellcheck",
+--     name = "shellcheck",
 --     args = { "--severity", "warning" },
 --   },
 -- }
